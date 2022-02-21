567f28d4564b325a6dd008a3daffc5324143ff35
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-02-21 07:44:28 +0000

    Add first table to describe pitch/phoneme relationship

---
+++ mutwo.ext-dfc22/mutwo/dfc22_parameters/constants.py
@@ -0,0 +1,32 @@
+XSAMPA_VOCAL_TO_FORMANT_FREQUENCY_TUPLE_DICT = {
+    # formant (xsampa notation): (f0, f1)
+    "i": (240, 2400),
+    "y": (235, 2100),
+    "e": (390, 2300),
+    "2": (370, 1900),
+    "E": (610, 1900),
+    "9": (585, 1710),
+    "a": (850, 1610),
+    "&": (820, 1530),
+    "A": (750, 940),
+    "Q": (700, 760),
+    "V": (600, 1170),
+    "O": (500, 700),
+    "7": (460, 1310),
+    "o": (360, 640),
+    "M": (300, 1390),
+    "u": (250, 595),
+    # difference reference!
+    "@": (665, 1772),
+}
+"""Average vowel formants for a male voice.
+
+Except for '@' all vowels are based on the following reference:
+
+Catford, J.C. (1988) A Practical Introduction to Phonetics,
+Oxford University Press, p. 161. ISBN 978-0198242178.
+
+The frequencies of '@' are based on the paper
+'The phonetics of schwa vowels' written by
+Edward Flemming.
+"""
